092d9968e50dbcecf4f9f2e45c1cd7fc8a830b4b
TakRaj <takraj@gmail.com>
2014-04-14 02:18:59 +0200

    Fejlődik a WP7 alkalmazás felülete...
    Signed-off-by: TakRaj <takraj@gmail.com>

---
+++ TransitPlannerService/TransitPlannerMobile/Logic/Utility.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace TransitPlannerMobile.Logic
+{
+    public abstract class Utility
+    {
+        /// <summary>
+        /// From: http://stackoverflow.com/a/4735854
+        /// </summary>
+        /// <param name="myMethod">Action to perform</param>
+        /// <param name="delayInMilliseconds">Delay in milliseconds</param>
+        public static void DelayedCall(Action myMethod, int delayInMilliseconds)
+        {
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.DoWork += (s, e) => Thread.Sleep(delayInMilliseconds);
+            worker.RunWorkerCompleted += (s, e) => myMethod.Invoke();
+            worker.RunWorkerAsync();
+        }
+    }
+}
--- TransitPlannerService/TransitPlannerMobile/MainPage.xaml.cs
+++ TransitPlannerService/TransitPlannerMobile/MainPage.xaml.cs
@@ -10,34 +10,201 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using Coding4Fun.Toolkit.Controls;
+using System.Threading;
+using System.Diagnostics;
+using System.Windows.Threading;
+using System.ComponentModel;
+using TransitPlannerMobile.Logic;
 
 namespace TransitPlannerMobile
 {
     public partial class MainPage : PhoneApplicationPage
     {
-        double InputHeight;
+        private double ReportTextBoxHeight;
+        private Action progressDialogActionOnCancel;
+        private bool progressDialogMustBeVisible;
 
         // Constructor
         public MainPage()
         {
             InitializeComponent();
+
+            ShowProgressDialog("Kommunikáció a szerverrel...");
+            Utility.DelayedCall(() => HideProgressDialog(), 5000);
+        }
+
+        private void ShowProgressDialog(string message, Action actionOnCancel = null)
+        {
+            lock (progressOverlay)
+            {
+                progressDialogMustBeVisible = true;
+                progressDialogActionOnCancel = actionOnCancel;
+            }
+
+            Dispatcher.BeginInvoke(() =>
+            {
+                lock (progressOverlay)
+                {
+                    lblProgressMessage.Text = message;
+
+                    if (progressDialogMustBeVisible)
+                    {
+                        progressOverlay.Show();
+                    }
+                }
+            });
+        }
+
+        private void HideProgressDialog(bool isCancelled = false)
+        {
+            lock (progressOverlay)
+            {
+                progressDialogMustBeVisible = false;
+            }
+
+            if (progressDialogActionOnCancel != null)
+            {
+                if (isCancelled)
+                {
+                    progressDialogActionOnCancel.Invoke();
+                }
+                
+                progressDialogActionOnCancel = null;
+            }
+            
+            Dispatcher.BeginInvoke(() => 
+            {
+                lock (progressOverlay)
+                {
+                    if (!progressDialogMustBeVisible)
+                    {
+                        progressOverlay.Hide();
+                    }
+                }
+            });
         }
 
         private void tbReportMessage_TextChanged(object sender, TextChangedEventArgs e)
         {
             Dispatcher.BeginInvoke(() =>
             {
-                double CurrentInputHeight = tbReportMessage.ActualHeight;
+                double CurrentReportTextBoxHeight = tbReportMessage.ActualHeight;
  
-                //after the user starts typing text, text will eventually wrap to the next line
-                //this ensures the textbox height doesnt sink below the bottom of the scrollviewer
-                if (CurrentInputHeight > InputHeight)
+                if (CurrentReportTextBoxHeight > ReportTextBoxHeight)
                 {
-                    svReport.ScrollToVerticalOffset(svReport.VerticalOffset + CurrentInputHeight - InputHeight);
+                    svReport.ScrollToVerticalOffset(svReport.VerticalOffset + CurrentReportTextBoxHeight - ReportTextBoxHeight);
                 }
  
-                InputHeight = CurrentInputHeight;
+                ReportTextBoxHeight = CurrentReportTextBoxHeight;
             });
+        }
+
+        private void btnClearReport_Click(object sender, RoutedEventArgs e)
+        {
+            Dispatcher.BeginInvoke(() =>
+            {
+                tbReportMessage.Text = string.Empty;
+                swIncludePosition.IsChecked = true;
+            });
+        }
+
+        private void btnSubmitReport_Click(object sender, RoutedEventArgs e)
+        {
+            ShowProgressDialog("Küldés...");
+            Utility.DelayedCall(() => HideProgressDialog(), 5000);
+        }
+
+        private void btnSelectReportType_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO
+        }
+
+        private void btnGetStopsByDistance_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO -- le lesz töltve előre, mert máshoz is kell
+        }
+
+        private void btnGetStopsByName_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO -- le lesz töltve előre, mert máshoz is kell
+        }
+
+        private void btnGetRoutesByName_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO -- le lesz töltve előre, mert máshoz is kell
+        }
+
+        private void btnGetRoutesByType_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO -- le lesz töltve előre, mert máshoz is kell
+        }
+
+        private void btnUseLocationFrom_Click(object sender, RoutedEventArgs e)
+        {
+            ShowProgressDialog("Helymeghatározás...");
+            Utility.DelayedCall(() => HideProgressDialog(), 5000);
+        }
+
+        private void btnUseLocationTo_Click(object sender, RoutedEventArgs e)
+        {
+            ShowProgressDialog("Helymeghatározás...");
+            Utility.DelayedCall(() => HideProgressDialog(), 5000);
+        }
+
+        private void btnNow_Click(object sender, RoutedEventArgs e)
+        {
+            Dispatcher.BeginInvoke(() =>
+            {
+                dpPlanDate.Value = DateTime.Now;
+                dpPlanTime.Value = DateTime.Now;
+            });
+        }
+
+        private void btnSelectDisabledRoutes_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO -- új képernyőn
+        }
+
+        private void btnSelectWalkingSpeed_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO
+        }
+
+        private void btnSelectMaxWaitingTime_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO
+        }
+
+        private void btnClearPlanning_Click(object sender, RoutedEventArgs e)
+        {
+            Dispatcher.BeginInvoke(() =>
+            {
+                tbPlanFrom.Text = string.Empty;
+                tbPlanTo.Text = string.Empty;
+
+                dpPlanDate.Value = DateTime.Now;
+                dpPlanTime.Value = DateTime.Now;
+
+                cbWheelchair.IsChecked = false;
+            });
+        }
+
+        private void btnMakePlan_Click(object sender, RoutedEventArgs e)
+        {
+            ShowProgressDialog("Keresés...");
+            Utility.DelayedCall(() => HideProgressDialog(), 5000);
+        }
+
+        private void PhoneApplicationPage_BackKeyPress(object sender, CancelEventArgs e)
+        {
+            lock (progressOverlay)
+            {
+                if (progressDialogMustBeVisible)
+                {
+                    e.Cancel = true;
+                }
+            }
         }
     }
 }
